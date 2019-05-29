e315321ad795f732251cedc4451f7f5abb8ee447
Galoopi <tiaanjvv@gmail.com>
2019-05-29 13:30:48 +0800

    update database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-animate-65663',
       user : 'tiaan',
       password : 'rohanjvv87',
       database : 'face-detection'
