b9ab57bb4c3b48b64a7a862791b849d35973d605
Galoopi <tiaanjvv@gmail.com>
2019-05-29 12:05:39 +0800

    fix server

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //server
-app.get('/',(req,res) => { res.send(database.users})
+app.get('/',(req,res) => { res.send('its working'})
 
 // signin
 app.post('/signin', signin.handleSignin(db, bcrypt))
